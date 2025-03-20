d9b1e5b21f7893e7f2b7611babd00ec8975a68ec
Kate465 <kategokeborate@gmail.com>
2025-03-20 15:29:46 +0000

    added github repository url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Katego and is {""}
+        <a href="https://github.com/Kate465/react-weather-project">
+          open sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
