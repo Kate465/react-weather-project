56f667eec762c5ad9d8a5109b2ec4db45264ae02
Kate465 <kategokeborate@gmail.com>
2025-03-21 00:40:41 +0000

    added API intergration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded by Katego and is {""}
           <a href="https://github.com/Kate465/react-weather-project">
